a2beb61c6c9739d4458c2fe883b2f983f6f5d907
Brady Peterson <bpeterson1663@gmail.com>
2021-10-22 22:54:23 -0500

    feat(project): display dummy data

--- src/pages/Projects.page.tsx
+++ src/pages/Projects.page.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
-
+import ProjectItem from '../components/project-item/project-item.component'
+const PROJECTS = [
+  {
+    id: 1,
+    name: 'Crown Clothing',
+    imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
+    url: 'https://bpeterson1663-crown-clothing.herokuapp.com/',
+  },
+  {
+    id: 2,
+    name: 'Scheduling App',
+    imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
+    url: 'https://when-i-work-challenge.herokuapp.com/',
+  },
+]
 const Projects = () => (
   <div className="page">
     <h1>Projects</h1>
-    <p>
-      Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content
-      Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content{' '}
-    </p>
+    {PROJECTS.map((project) => (
+      <ProjectItem project={project} key={project.id} />
+    ))}
   </div>
 )
 
